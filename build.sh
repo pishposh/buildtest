0d4751051b1343c546d9a9d407c6341c0d3a5d82
Kentaro Kaji <kentaro.kaji@nytimes.com>
2016-01-28 03:47:17 -0500

    make build.sh change blargh

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ echo "garbage" >>ignored-build-detritus
 mkdir -p ignored-build-detritus-dir
 echo "more garbage" >>ignored-build-detritus-dir/garb1
 echo "yet more garbage" >>ignored-build-detritus-dir/garb2
+echo "ch-ch-ch-changes" >>blargh
