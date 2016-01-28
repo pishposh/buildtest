91c90b71afd56e14be8882e828447e0f01e9f602
Kentaro Kaji <kentaro.kaji@nytimes.com>
2016-01-28 03:48:34 -0500

    ...and rm argh

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ echo "garbage" >>ignored-build-detritus
 mkdir -p ignored-build-detritus-dir
 echo "more garbage" >>ignored-build-detritus-dir/garb1
 echo "yet more garbage" >>ignored-build-detritus-dir/garb2
+
 echo "ch-ch-ch-changes" >>blargh
+rm -f argh
