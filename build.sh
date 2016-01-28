7c1ba3be6e238a78ce33b03221d9f2a7bbff04ea
Kentaro Kaji <kentaro.kaji@nytimes.com>
2016-01-28 03:18:16 -0500

    initial

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "garbage" >>ignored-build-detritus
+mkdir -p ignored-build-detritus-dir
+echo "more garbage" >>ignored-build-detritus-dir/garb1
+echo "yet more garbage" >>ignored-build-detritus-dir/garb2
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p node_modules
+echo "foo" >>node_modules/module1
+
+mkdir -p tests/node_modules
+echo "bar" >>tests/node_modules/module2
